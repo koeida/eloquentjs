1f787030dbb2c0235f599e9647ac0222cb665af4
Keegan <keegan@twinoaks.org>
2021-06-29 08:01:32 -0400

    weird box exercise complete

--- ch08_exercises.js
+++ ch08_exercises.js
@@ -28,5 +28,61 @@ function reliableMultiply(a, b) {
   }
 }
 
-console.log(reliableMultiply(8, 8));
-// → 64
+console.assert(reliableMultiply(8, 8) === 64);
+
+
+// -------------------------------------------------------
+
+/*
+It is a box with a lock. There is an array in the box, but you can get at it
+only when the box is unlocked. Directly accessing the private _content property
+is forbidden.
+
+Write a function called withBoxUnlocked that takes a function value as
+argument, unlocks the box, runs the function, and then ensures that the box is
+locked again before returning, regardless of whether the argument function
+returned normally or threw an exception.
+
+For extra points, make sure that if you call withBoxUnlocked when the box is
+already unlocked, the box stays unlocked.
+*/
+
+const box = {
+  locked: true,
+  unlock() { this.locked = false; },
+  lock() { this.locked = true;  },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  }
+};
+
+function withBoxUnlocked(body) {
+  let locked = box.locked;
+  box.unlock();
+  try {
+    body();
+  } finally {
+    if (locked) box.lock();
+    else box.unlock();
+  }
+}
+
+withBoxUnlocked(function() {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function() {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised: " + e);
+}
+console.assert(box.locked);
+
+//try out the if-unlocked-stay-unlocked code
+box.unlock();
+withBoxUnlocked(() => box.content.push('diamond'));
+console.assert(!box.locked);
